ab833e3ef8417fdecd179f550ce956951e699ada
Evi1n <evilnn.19@gmail.com>
2022-07-12 11:39:09 +0300

    python

--- day6/reeborgs_world.py
+++ day6/reeborgs_world.py
@@ -1,7 +1,2 @@
 # solving reeborg's world levels and debugging the problem 
 
-def nametag(first_name, last_name):
-    return("{} {}.".format(first_name, last_name[0]))
-
-print(nametag("evin", "urun"))
-print(nametag("dilan", "urun"))
