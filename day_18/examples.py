916fbd2038cb0ca1c7b1a3de18aef5c0c8d70af8
Evi1n <evilnn.19@gmail.com>
2022-07-18 18:24:51 +0300

    python

---
+++ day_18/examples.py
@@ -0,0 +1,144 @@
+import turtle
+import random
+
+""" DRAW A SQUARE
+
+square = Turtle()
+
+square.pensize(3)
+for i in range(4):
+    square.forward(100)
+    square.left(90)
+"""
+
+
+""" DRAW A DASHED LINE
+
+dashed_line = Turtle()
+dashed_line.pensize(2)
+
+for i in range(10):
+    dashed_line.forward(10)
+    dashed_line.penup()
+    dashed_line.forward(10)
+    dashed_line.pendown()
+"""
+
+
+""" DRAW A SHAPES
+
+shapes = Turtle()
+
+shapes.pensize(2)
+shapes.pencolor("blue")
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for i in range(num_sides):
+        shapes.forward(100)
+        shapes.right(angle)
+
+for i in range(3, 11):
+    draw_shape(i)
+    
+shapes.exitonclick()
+"""
+
+
+""" MY CODE FOR RANDOM WALK
+
+def go_right(walker, step):
+    walker.forward(step)
+    
+def go_up(walker, step):
+    walker.left(90)
+    walker.forward(step)
+    
+def go_left(walker, step):
+    walker.backward(step)
+
+def go_down(walker, step):
+    walker.right(90)
+    walker.forward(step)
+            
+def make_random_walk(step_number, step_size):
+    random_walk = turtle.Turtle()
+    random_walk.pensize(3)
+    
+    colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+    move_dict = {1: go_up,
+                 2: go_right,
+                 3: go_left,
+                 4: go_down
+                 }
+    for _ in range(step_number):
+        walker = move_dict[random.randint(1, 4)]
+        random_walk.color(random.choice(colours))
+        walker(random_walk, step_size)
+         
+w = turtle.Screen()
+w.setup(width=432,height=768)
+make_random_walk(100, 30)
+w.exitonclick()
+""" 
+
+""" MY CODE 2 FOR RANDOM WALK
+
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    color = r,g,b
+    return color
+
+def random_walk():
+    walk = turtle.Turtle()
+    walk.pensize(6)
+    walk.hideturtle()
+
+    directions = [0, 90, 180, 270]
+
+    for _ in range(100):
+        walk.setheading(random.choice(directions))
+        walk.pencolor(random_color())
+        walk.forward(20)
+
+w = turtle.Screen()
+w.setup(width=432,height=768)
+w.bgcolor("black")
+random_walk()
+w.exitonclick()
+
+"""
+
+"""DRAW A SPIROGRAPH (CIRCLE)
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    color = r,g,b
+    return color
+
+def spirograph():
+    draw = turtle.Turtle()
+    draw.hideturtle()
+    draw.speed("fastest")
+
+    for _ in range(100):
+        draw.color(random_color())
+        draw.circle(100)
+        draw.left(10)
+
+
+w = turtle.Screen()
+w.setup(width=432,height=768)
+w.bgcolor("black")
+spirograph()
+w.exitonclick()
+"""
---
+++ day_18/main.py
@@ -0,0 +1,43 @@
+import turtle
+import random
+
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    color = r,g,b
+    return color
+
+screen = turtle.Screen()
+screen.bgcolor("black")
+screen.setup(width=432, height=768)
+
+dots = turtle.Turtle()
+dots.hideturtle()
+dots.speed("fast")
+dots.penup()
+dots.setheading(225)
+dots.forward(250)
+dots.setheading(0)
+
+number_of_dots = 110
+
+def draw():
+
+    for _ in range(1, number_of_dots+1):
+        dots.color(random_color())
+        dots.dot(20)
+        dots.forward(40)
+
+        if _ % 10 == 0:
+            dots.setheading(90)
+            dots.forward(40)
+            dots.setheading(180)
+            dots.forward(400)
+            dots.setheading(0)   
+draw()
+      
+screen.exitonclick()
