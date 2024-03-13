f98e366457c7ce35fdc2dedb2a98e85286222420
Evin <89968212+Evi1n@users.noreply.github.com>
2024-03-13 17:19:59 +0300

    Update calculator.py

--- day_10/calculator.py
+++ day_10/calculator.py
@@ -42,11 +42,11 @@ def calculator():
         if operation == "+":
             print("{} + {} = {}".format(num1, num2, add(num1, num2)))
         elif operation == "-":
-            print("{} + {} = {}".format(num1, num2, subtract(num1, num2)))
+            print("{} - {} = {}".format(num1, num2, subtract(num1, num2)))
         elif operation == "*":
-            print("{} + {} = {}".format(num1, num2, multiply(num1, num2)))
+            print("{} * {} = {}".format(num1, num2, multiply(num1, num2)))
         elif operation == "/":
-            print("{} + {} = {}".format(num1, num2, divide(num1, num2)))
+            print("{} / {} = {}".format(num1, num2, divide(num1, num2)))
         else:
             print("Please pick an valid operation!")
     
